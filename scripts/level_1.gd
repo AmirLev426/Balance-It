ae9dacf7072c3ebea90a832987636df7c610b0a4
AmirLev426 <amirbou426@gmail.com>
2025-11-28 14:46:59 +0100

    Spawner
    - Added a spawner

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var spawner: Button = $spawner
+
+var movable_block = preload("res://scenes/movable_block.tscn")
+
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	call_deferred("queue_free")
 	call_deferred("change_scene")
@@ -7,3 +11,11 @@ func _on_area_2d_body_entered(_body: Node2D) -> void:
 
 func change_scene() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+
+func _on_spawner_pressed() -> void:
+	inst(spawner.get_global_position())
+
+func inst(pos): #on button press
+	var instance = movable_block.instantiate()
+	instance.position = pos
+	add_child(instance)
