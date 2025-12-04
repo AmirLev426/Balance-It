b88bffe3c0aceacc5dbf68b63ed1b8252b9a4a11
AmirLev426 <amirbou426@gmail.com>
2025-12-04 20:02:57 +0100

    pause_menu fixes
    fixed :
    - block stop from clicking on pause buttons
    - pause really pause the game

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var audio: Node2D = $Audio
-
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 var is_paused = false
 
 func _ready():
@@ -11,10 +11,14 @@ func _ready():
 func resume():
 	$AnimationPlayer.play_backwards("blur")
 	is_paused = false
+	get_tree().paused = false
+	canvas_layer.layer = 0
 
 func pause():
 	$AnimationPlayer.play("blur")
 	is_paused = true
+	get_tree().paused = true
+	canvas_layer.layer = 100
 
 func testEsc():
 	if Input.is_action_just_pressed("escape"):
@@ -28,15 +32,17 @@ func _on_resume_pressed() -> void:
 	resume()
 
 func _on_restart_pressed() -> void:
+	resume()
 	audio.random_sfx()
 	get_tree().reload_current_scene()
-	resume()
 
 func _on_change_level_pressed() -> void:
+	resume()
 	audio.random_sfx()
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 func _on_main_menu_pressed() -> void:
+	resume()
 	audio.random_sfx()
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
